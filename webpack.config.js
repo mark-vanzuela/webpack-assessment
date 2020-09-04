3b73bb86dee95cc12455df7e7e130f604edd319d
mark vanzuela <mark.vanzuela@devpartners.co>
2020-09-04 16:02:59 +0800

    Initial Commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
 
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const { config } = require('process');
+
+module.exports = function (env) {
+  const isDevelopment = env === 'development';
+  console.log(
+    `This is a ${isDevelopment ? 'development' : 'production'} build`
+  );
+  const baseConfig = {
+    entry: './app/app.js',
+    output: {
+      path: path.resolve(__dirname, 'app/dist'),
+      publicPath: '/dist/',
+      filename: 'app.bundle.js',
+    }
+  }
+}
